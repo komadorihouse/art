55de35ec0c0e6c28b7c1136a8e4bb74f2993c3c1
komadorihouse <komadorihouse@yahoo.co.jp>
2022-09-23 23:31:43 +0900

    emailを一意とする処理

--- protected/models/SignUpForm.php
+++ protected/models/SignUpForm.php
@@ -43,16 +43,35 @@ class SignUpForm extends CFormModel
 	public function save() {
 		
 		if ( !$this->hasErrors() ) {
-			
-			$tbl_user = new TblUser();
-			$tbl_user->username = $this->username;
-			$tbl_user->password = md5($this->password);
-			$tbl_user->email = $this->email;
-			$tbl_user->sait = $this->sait;
-			$tbl_user->profile = $this->profile;
-			$tbl_user->save();
 
-			return $tbl_user;
+			$is_error = false;
+
+			//tbl_userテーブルを入力されたアドレスで検索
+			$model = TblUser::model()->find(array(
+				'condition' => "email = '{$this->email}' "
+			));
+
+			//検索結果が存在する場合はエラーを表示
+			if($model){
+				//$this->addErrorはフォームにエラーを挿入する。第一引数はエラー対象項目名、第二引数はエラー内容
+				$this->addError('email','このメールアドレスは既に使用されています。');
+				//is_errorを真にする。
+				$is_error = true;
+			}
+			
+			//is_errorが偽の時のみ内容を保存する。
+			if(!$is_error){
+				$tbl_user = new TblUser();//新規にテーブルに保存する場合はテーブルのインスタンスを呼ぶ。
+				//テーブル名->カラム名 = フォームで送られてきたデータ。
+				$tbl_user->username = $this->username;
+				$tbl_user->password = md5($this->password);//ms5はphpの関数ハッシュ化し、内容を隠すことができる。
+				$tbl_user->email = $this->email;
+				$tbl_user->sait = $this->sait;
+				$tbl_user->profile = $this->profile;
+				$tbl_user->save();//$フォーム->save();でフォームにセットされた値をテーブルに保存できる。
+
+				return $tbl_user;//SiteControllerに保存したデータが返され、コントローラー側で$dataに代入される。
+			}
 		}
 		return false;
 	}
